8bf5485523d07ee5ab28e018616e386f79cb6cba
naoya.matsumoto <naowork12@gmail.com>
2022-07-13 07:43:21 +0000

    for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,6 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'naoya-backet' # バケット名
-    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], 
